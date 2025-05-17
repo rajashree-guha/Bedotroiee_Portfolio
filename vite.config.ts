00a4637c28adeecf31532beb5056d6473a2e636b
rajashree-guha <rajashree.625.guha@gmail.com>
2025-05-17 22:07:58 +0530

    fixed error

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/Bedotroiee_Portfolio/',
+  base: mode === 'development' ? '/' : '/Bedotroiee_Portfolio/',
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    ...(mode === 'development' ? [componentTagger()] : []),
   ].filter(Boolean),
   resolve: {
     alias: {
